00034e4aa24699f306c9be63950e9f595c453b89
risha016 <137895362+risha016@users.noreply.github.com>
2023-06-27 15:08:04 +0300

    Create random.js

---
+++ random.js
@@ -0,0 +1,7 @@
+// Генерация случайного целого числа в диапазоне от 1 до 10
+var random_number = Math.floor(Math.random() * 10) + 1;
+console.log(random_number);
+
+// Генерация случайного числа с плавающей запятой в диапазоне от 0 до 1
+var random_float = Math.random();
+console.log(random_float);
